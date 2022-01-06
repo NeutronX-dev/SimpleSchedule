4167e6b47b903b211f7fbde87b53c66f580d4f4a
Neutron <32855909+NeutronX-dev@users.noreply.github.com>
2022-01-06 08:42:06 -0500

    First Commit

---
+++ src/ConfigLoader/main.go
@@ -0,0 +1,95 @@
+package ConfigLoader
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"time"
+)
+
+type Schedule struct {
+	Title string `json:"title"`
+	Time  int64  `json:"time"`
+}
+
+func remove(slice []*Schedule, s int) []*Schedule {
+	if len(slice) == 1 {
+		return make([]*Schedule, 0)
+	}
+	return append(slice[:s], slice[s+1:]...)
+}
+
+func (schedule *Schedule) HasPassed() bool {
+	return time.Now().UnixMilli() >= schedule.Time
+}
+
+type Schedules struct {
+	data []*Schedule
+	path string
+}
+
+func (schedules *Schedules) ScheduleAmounts() int {
+	return len(schedules.data)
+}
+
+func (schedules *Schedules) Access(index int) *Schedule {
+	return schedules.data[index]
+}
+
+func (schedules *Schedules) HasPassed() []*Schedule {
+	var res []*Schedule = make([]*Schedule, 0)
+	for _, v := range schedules.data {
+		if time.Now().UnixMilli() >= v.Time {
+			res = append(res, v)
+		}
+	}
+	return res
+}
+
+func (schedules *Schedules) RemovePassed() error {
+	for i, v := range schedules.data {
+		if time.Now().UnixMilli() >= v.Time {
+			schedules.data = remove(schedules.data, i)
+		}
+	}
+	return schedules.Save()
+}
+
+func (schedules *Schedules) AddSchedule(Title string, UnixTime int64) error {
+	var NewSchedule *Schedule = &Schedule{
+		Title: Title,
+		Time:  UnixTime,
+	}
+	schedules.data = append(schedules.data, NewSchedule)
+
+	return schedules.Save()
+}
+
+func (schedules *Schedules) Save() error {
+	data, err := json.MarshalIndent(schedules.data, "", "\t")
+	if err != nil {
+		return err
+	}
+
+	err = ioutil.WriteFile(schedules.path, data, 0777)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func ReadCFG(path string) (Schedules, error) {
+	var res Schedules
+	res.path = path
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return res, err
+	}
+	err = json.Unmarshal(data, &res.data)
+	if err != nil {
+		return res, err
+	}
+
+	return res, nil
+}
